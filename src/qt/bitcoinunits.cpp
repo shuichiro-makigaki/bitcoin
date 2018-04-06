841a5349563a3ed3d532e8ccf0d1b7aabdd04657
Shuichiro MAKIGAKI <shuichiro.makigaki@gmail.com>
2018-04-06 18:25:48 +0900

    Switch BTC to ATP

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -40,9 +40,9 @@ QString BitcoinUnits::longName(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("BTC");
-    case mBTC: return QString("mBTC");
-    case uBTC: return QString::fromUtf8("µBTC (bits)");
+    case BTC: return QString("ATP");
+    case mBTC: return QString("mATP");
+    case uBTC: return QString::fromUtf8("µATP (bits)");
     default: return QString("???");
     }
 }
